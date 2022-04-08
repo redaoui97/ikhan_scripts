ff07837bfef16b7ed5f09921a6814c1d81cc8737
Nabil Reda <redanabil14@gmail.com>
2022-04-08 21:50:39 +0000

    working good

--- script.sh
+++ script.sh
@@ -1,7 +1,30 @@
-#/bin/ksh
-while [[ $1 -ge 0 ]]
+#/bin/bash
+count=$1
+if [ $2 == "m" ]
+	then
+		echo "locking screen in: "$count " minutes"
+		count=$(( $count * 60 ))
+fi
+if [ $2 == "h" ]
+	then
+		echo "locking screen in: "$count " hours"
+		count=$(( $count * 60 * 60 ))
+fi
+if [ $2 == "s" ]
+	then
+		echo "locking screen in: "$count " seconds"
+fi
+while [[ count -ge 0 ]]
 do
         sleep 1
-        countdown=$(( $1 - 1 ))
+        count=$(( $count - 1 ))
+		if (( $count < 0))
+			then
+				open https://www.youtube.com/watch?v=A9GMqjLbIpQ
+		fi
 done
 pmset displaysleepnow
+while (( $count < 10 ))
+do
+	osascript -e "set Volume 10"
+done
