994305312420c45a54c4b0c629e7395381f51b7a
readoui <redanabil14@gmail.com>
2023-07-23 19:28:38 +0100

    fixed something small

--- simple_level_lbenj/trigger_lbenj.sh
+++ simple_level_lbenj/trigger_lbenj.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-echo $1 | nc "$(ifconfig | grep -B1 "status: active" | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | awk '{print $2}')" 6969
+echo $1 |nc "$(ifconfig | grep -B1 "status: active" | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | awk '{print $2}')" 6969
